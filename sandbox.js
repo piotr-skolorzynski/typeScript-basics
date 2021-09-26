714918062fd5d090367a914e728ac379bce25987
Piotr <p.skolorzynski@gmail.com>
2021-09-26 20:11:02 +0200

    modify sandbox.ts add examples how to explicit type values

--- sandbox.js
+++ sandbox.js
@@ -1,31 +1,34 @@
-//arrays
-var names = ['luigi', 'mario', 'yoshi']; // we can use only string type inside array, also we cannot change names variable to other type f.e. string names = 'milk'
-names.push('toad');
-// names.push(3); cannot add item with different type
-// names[0] = 3;  overwrite also imposible
-var numbers = [10, 20, 30, 40];
-numbers.push(25);
-// numbers.push('mleko'); different types
-// numbers[1] = 'mario'; the same error
-// if we want to have array with different types it is needed to declare that
-var mixed = ['ken', 4, 'chun-li', 8, 9]; //now typeScript knows that we want to use two types in array
-mixed.push('ryu');
-mixed.push(10);
-mixed[0] = 3;
-//objects
-var ninja = {
-    name: 'mario',
-    belt: 'black',
-    age: 30 //this always number
-};
-//ninja variable must always be an object
-ninja.age = 40;
-ninja.name = 'ryu';
-// ninja.age = '30'; cannot change type from number to string
-// ninja.skills = ['fighting', 'sneaking']; cannot add another object's property because it wasn't defined at the beginning
-//it is possible to overwrite whole object but cannot change properties it must be exact same object structure
-ninja = {
+//explicit types
+var character;
+var age;
+var isLoggedIn;
+// age = 'luigi'; impossible cause we declared type number
+age = 30;
+// isLoggedIn = 25; wrong type, should be boolean
+isLoggedIn = true;
+// arrays
+// let ninjas: string[]; //array of strings only sometimes it is best to also initialize with empty array to possible using array methods on this variable let ninjas: string[] = []
+var ninjas = [];
+// ninjas = [10, 20]; //wrong types
+ninjas = ['yoshi', 'mario'];
+ninjas.push('shaun');
+//union types
+var mixed = [];
+mixed.push('hello');
+mixed.push(20);
+// mixed.push(true); wrong type, not declared
+console.log(mixed);
+var uid;
+uid = '123';
+uid = 123;
+// uid = false; wrong type
+// objects
+var ninjaOne;
+ninjaOne = {
     name: 'yoshi',
-    belt: 'orange',
-    age: 40
+    age: 30
 };
+// ninjaOne = 'milk' wrong type
+// ninjaOne = []; will work because it is an object
+var ninjaTwo; //we declare not only object but also its type
+ninjaTwo = { name: 'mario', age: 20, beltColor: 'black' };
--- sandbox.ts
+++ sandbox.ts
@@ -1,37 +1,49 @@
-//arrays
-let names = ['luigi', 'mario', 'yoshi'];// we can use only string type inside array, also we cannot change names variable to other type f.e. string names = 'milk'
+//explicit types
+let character: string;
+let age: number;
+let isLoggedIn: boolean;
 
-names.push('toad');
-// names.push(3); cannot add item with different type
-// names[0] = 3;  overwrite also imposible
+// age = 'luigi'; impossible cause we declared type number
+age = 30;
 
-let numbers = [10, 20, 30, 40];
-numbers.push(25);
-// numbers.push('mleko'); different types
-// numbers[1] = 'mario'; the same error
+// isLoggedIn = 25; wrong type, should be boolean
+isLoggedIn = true;
 
-// if we want to have array with different types it is needed to declare that
-let mixed = ['ken', 4, 'chun-li', 8, 9]; //now typeScript knows that we want to use two types in array
-mixed.push('ryu');
-mixed.push(10);
-mixed[0] = 3;
+// arrays
+// let ninjas: string[]; //array of strings only sometimes it is best to also initialize with empty array to possible using array methods on this variable let ninjas: string[] = []
 
-//objects
-let ninja = {
-    name: 'mario', //this attribute must always be string
-    belt: 'black',
-    age: 30 //this always number
-};
-//ninja variable must always be an object
+let ninjas: string[] = []
 
-ninja.age = 40;
-ninja.name = 'ryu';
-// ninja.age = '30'; cannot change type from number to string
-// ninja.skills = ['fighting', 'sneaking']; cannot add another object's property because it wasn't defined at the beginning
-//it is possible to overwrite whole object but cannot change properties it must be exact same object structure
-ninja = {
+// ninjas = [10, 20]; //wrong types
+ninjas = ['yoshi', 'mario'];
+ninjas.push('shaun');
+
+
+//union types
+let mixed: (string|number)[] = [];
+mixed.push('hello');
+mixed.push(20);
+// mixed.push(true); wrong type, not declared
+console.log(mixed)
+
+let uid: string|number;
+uid = '123';
+uid = 123;
+// uid = false; wrong type
+
+// objects
+let ninjaOne: object;
+ninjaOne = {
     name: 'yoshi',
-    belt: 'orange',
-    age: 40,
-    // skills: [] impossible to add
+    age: 30
 }
+// ninjaOne = 'milk' wrong type
+// ninjaOne = []; will work because it is an object
+
+let ninjaTwo: {
+    name: string,
+    age: number,
+    beltColor: string
+}; //we declare not only object but also its type
+
+ninjaTwo = { name: 'mario', age: 20, beltColor: 'black'};
